dd54b86a9ef703888ee7ae9cb0b509a44f81b342
renato.monteiro <renato.monteiro@conam.com.br>
2020-06-01 20:52:55 -0300

    Change

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.TodoList.as_view(), name='list_to_do'),
     path('todo/new/', views.TodoCreate.as_view(), name='new_to_do'),
-    path('completed/<int:pk>/', views.completed, name='completed'),
+    path('completed/<int:pk>/', views.completed, name='completed x'),
 ]
 
